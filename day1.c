58367608cc84d4c8b4c2e998102c8f40dc8ee6db
Abhishek393913 <abhishekgupta80228@gmail.com>
2025-11-28 14:19:03 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    printf("Name: Abhishek Gupta\n");
+    printf("SAP ID: 590029650\n");
+    return 0;
+}
---
+++ day2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//aria of square
+int main(){
+     float redius;
+    printf("enter redius");
+    scanf("%f", &redius);
+
+    printf("aria is : %f",3.14 * redius * redius );
+    return 0;
+}
